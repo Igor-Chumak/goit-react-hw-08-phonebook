613d34eac3391991fc880c24408dfa9809091145
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-31 17:59:29 +0300

    pause

--- src/components/AuthNav/AuthNav.styled.jsx
+++ src/components/AuthNav/AuthNav.styled.jsx
@@ -1,11 +1,17 @@
-.link {
-  display: inline-block;
-  text-decoration: none;
-  padding: 12px;
-  font-weight: 700;
-  color: #2a363b;
-}
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
-.link.active {
-  color: #e84a5f;
-}
+export const NavLinkStyled = styled(NavLink)`
+  /* display: inline-block; */
+  padding: 8px 12px;
+  border-radius: 8px;
+  color: black;
+  font-weight: 700;
+  text-decoration: none;
+  transition: 0.1s cubic-bezier(0.7, 0.98, 0.86, 0.98);
+
+  &.active {
+    color: red;
+    background-color: #e9e8e8;
+  }
+`;
